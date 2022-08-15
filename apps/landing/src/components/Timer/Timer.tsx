f63ac2fe49d3f4835519a7d4ea49630d121bc3b1
Peregrinus <zakharov.mark22@gmail.com>
2022-08-15 18:29:38 +0300

    timer: add timer

--- apps/landing/src/components/Timer/Timer.tsx
+++ apps/landing/src/components/Timer/Timer.tsx
@@ -6,44 +6,22 @@ interface TimerProps {
 }
 
 export const Timer: React.FC<TimerProps> = ({ timerName, timer }) => {
-  const [totalSeconds, setTotalSeconds] = useState(0);
-  const [timerOn, setTimeOn] = React.useState(false);
-  // var totalSeconds = 0;
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
-    let interval = null;
-
-    if (timerOn) {
-      interval = setInterval(() => {
-        setTotalSeconds((prevTime) => prevTime + 10);
-      }, 10);
-    } else {
+    const interval = setInterval(() => setTime(time + 1), 1000);
+    return () => {
       clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [timerOn]);
+    };
+  });
 
-  // function setTime() {
-  //   ++totalSeconds;
-  //   secondsLabel.innerHTML = pad(totalSeconds % 60);
-  //   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-  // }
-  // function pad(val) {
-  //   var valString = String(val);
-  //   if (valString.length < 2) {
-  //     return "0" + valString;
-  //   } else {
-  //     return valString;
-  //   }
-  // }
-  const convertToHHMMSS = () => {
-    const valueString = '';
+  const convertTimeToHHMMSS = (time: number) => {
+    return new Date(1970, 0, 0, 0, 0, time, 0).toLocaleTimeString();
   };
   return (
     <>
-      <button onClick={() => setTimeOn(true)}>start</button>
       <h1>{timerName}</h1>
-      <p>{totalSeconds}</p>
+      <p>{convertTimeToHHMMSS(time)}</p>
     </>
   );
 };
